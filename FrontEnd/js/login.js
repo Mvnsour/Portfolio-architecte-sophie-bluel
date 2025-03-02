d73fc98ec01e8cea2a486af023e69c36f4dc0f86
Mansour Mbengue <mbgmansour13@gmail.com>
2025-03-03 00:14:40 +0100

    refactor: login form because I had errors in the console

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,23 +1,31 @@
 const userLogin = "http://localhost:5678/api/users/login";
+const loginForm = document.getElementById("loginForm");
+const email = document.getElementById("email"); ;
+const password = document.getElementById("password");
+const errorMessage = document.querySelector(".error-message");
 
-document.getElementById("loginForm").addEventListener("submit", onSubmit);
+// Check if the elements exist
+if (!loginForm || !email || !password || !errorMessage) {
+    console.error("Erreur : Un ou plusieurs éléments du formulaire sont introuvables !");
+} else {
+    console.log("Tous les éléments du formulaire sont bien détectés !");
 
-async function onSubmit(e) {
-    e.preventDefault();
-    const user = {
-        email: document.getElementById("email").value,
-        password: document.getElementById("password").value,
-    };
+    loginForm.addEventListener("submit", function(event) {
+        event.preventDefault();
+        console.log("Formulaire soumis");
 
-    let response = await fetch(userLogin, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
+        // Check if the inputs are not null before accessing their value
+        // I am obliged to use the instanceof operator to avoid a type error in the console
+        const email = emailInput instanceof HTMLInputElement ? emailInput.value : "";
+        const password = passwordInput instanceof HTMLInputElement ? passwordInput.value : ""; 
+
+        console.log("Email saisi :", email);
+        console.log("Mot de passe saisi :", password);
+
+        fetch("http://localhost:5678/api/users/login", { 
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({email, password})
+        })
     })
-
-    let result = await response.json();
-    console.log(result);
-    alert(result.message);
 }
