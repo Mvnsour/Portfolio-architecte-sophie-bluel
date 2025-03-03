f8c501311cd675eb2752e12e3e4350578c8b51c7
Mansour Mbengue <mbgmansour13@gmail.com>
2025-03-03 11:50:51 +0100

    refactor: improve variable naming and enhance fetch response handling in login.js

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,17 +1,17 @@
 const userLogin = "http://localhost:5678/api/users/login";
 const loginForm = document.getElementById("loginForm");
-const email = document.getElementById("email"); ;
-const password = document.getElementById("password");
+const emailInput = document.getElementById("email"); ;
+const passwordInput = document.getElementById("password");
 const errorMessage = document.querySelector(".error-message");
 
 // Check if the elements exist
-if (!loginForm || !email || !password || !errorMessage) {
+if (!loginForm || !emailInput || !passwordInput || !errorMessage) {
     console.error("Erreur : Un ou plusieurs éléments du formulaire sont introuvables !");
 } else {
     console.log("Tous les éléments du formulaire sont bien détectés !");
 
     loginForm.addEventListener("submit", function(event) {
-        event.preventDefault();
+        event.preventDefault(); // avoid the default behavior of the form
         console.log("Formulaire soumis");
 
         // Check if the inputs are not null before accessing their value
@@ -22,10 +22,13 @@ if (!loginForm || !email || !password || !errorMessage) {
         console.log("Email saisi :", email);
         console.log("Mot de passe saisi :", password);
 
-        fetch("http://localhost:5678/api/users/login", { 
+        fetch(userLogin, { 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({email, password})
-        })
+        }).then(response => {
+                console.log("Réponse brute du serveur :", response);
+                return response.json();
+            })
     })
-}
+} 
