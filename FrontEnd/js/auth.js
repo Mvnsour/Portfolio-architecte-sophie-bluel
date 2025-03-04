2058ca679465d30325c3ab7f5044cf390ca304d0
Mansour Mbengue <mbgmansour13@gmail.com>
2025-03-04 22:26:17 +0100

    feat(auth): implement user authentication function for login

---
+++ FrontEnd/js/auth.js
@@ -0,0 +1,26 @@
+const userLogin = "http://localhost:5678/api/users/login";
+
+/**
+ * User authentication
+ * @param {string} email - User email
+ * @param {string} password - User password
+ */
+
+export async function checkAuth(email, password) {
+  try {
+    const response = await fetch(userLogin, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({email, password})
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message);
+    }
+    return data; // return the token if successful
+  } catch (error) {
+    console.error("error auth: ", error);
+    throw error; // spread the error to handle it in the login.js file
+  }
+}
