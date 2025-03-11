d22e3105fca80ebe6ad04d0588fa75046ad57ecf
Mansour Mbengue <mbgmansour13@gmail.com>
2025-03-11 18:41:44 +0100

    feat(login): enhance login functionality with token management and UI updates but not finished

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -4,12 +4,47 @@ const loginForm = document.getElementById("loginForm");
 const emailInput = document.getElementById("email"); ;
 const passwordInput = document.getElementById("password");
 const errorMessage = document.querySelector(".error-message");
+const loginLink = document.getElementById("login-page"); 
+const filtersContainer = document.getElementById("filters-container");
+const editButton = document.getElementById("edit-btn");
 
 // Check if the elements exist
-if (!loginForm || !emailInput || !passwordInput || !errorMessage) {
+if (!loginForm || !emailInput || !passwordInput || !errorMessage || !loginLink) {
     console.error("Erreur : Un ou plusieurs éléments du formulaire sont introuvables !");
 } else {
     console.log("Tous les éléments du formulaire sont bien détectés !");
+
+    // Fonction pour vérifier si l'utilisateur est connecté
+const updateLogin = () => {
+    const token = localStorage.getItem("authToken");
+    if (token) {
+        if (loginLink instanceof HTMLAnchorElement) {
+            loginLink.href = "#"; // Pas d'erreur
+/*             loginLink.href = "login.html"; // Pas d'erreur
+ */        }
+            loginLink.onclick = () => {
+                localStorage.removeItem("authToken");
+                window.location.href = "login.html"; // Rediriger vers la page de connexion
+            };
+        } else {
+            loginLink.textContent = "login";
+            // @ts-ignore
+            loginLink.href = "login.html";
+            loginLink.onclick = null; // Retirer le gestionnaire de clic
+        }
+
+        // Masquer les filtres et afficher le bouton "modifier" si l'utilisateur est connecté
+        if (filtersContainer) {
+            filtersContainer.style.display = token ? "none" : "block";
+        }
+
+        if (editButton) {
+            editButton.style.display = token ? "block" : "none";
+        }
+    };
+  
+  updateLogin();
+  // Vérifier si l'utilisateur est déjà connecté au chargement de la page
 
     loginForm.addEventListener("submit", async(event) => {
         event.preventDefault(); // avoid the default behavior of the form
@@ -27,11 +62,13 @@ if (!loginForm || !emailInput || !passwordInput || !errorMessage) {
         .then(data => {
             if (data.token) { // If authentication is successful
                 localStorage.setItem('authToken', data.token); // store the token in the local storage
+                localStorage.setItem('isLoggedIn', "true"); // store the login status in the local storage
                 window.location.href = 'index.html'; // redirect to the index page
             }
         }).catch(error => {
             console.error("Erreur d'authentification :", error);
             errorMessage.textContent = "Email ou mot de passe incorrect.";
         });
-        })
-    }
+    });
+
+}
