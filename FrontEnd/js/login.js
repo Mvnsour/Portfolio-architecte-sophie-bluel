52f094b1c5aeeadd5965f7808a6a1774d23b60a8
Mansour Mbengue <mbgmansour13@gmail.com>
2025-04-12 18:23:20 +0200

    feat(login): refactor login form initialization to use DOMContentLoaded event

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,29 +1,35 @@
 import { checkAuth } from "./auth.js";
 
-const loginForm = document.getElementById("loginForm");
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
-const errorMessage = document.querySelector(".error-message");
+// Method 1: Using DOMContentLoaded event
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Page fully loaded');
 
-if (!loginForm || !emailInput || !passwordInput || !errorMessage) {
-  console.error("Error: One or more elements are missing in the DOM.");
-} else {
-  loginForm.addEventListener("submit", async (event) => {
-    event.preventDefault();
+  const loginForm = document.getElementById("loginForm");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+  const errorMessage = document.querySelector(".error-message");
+
+  if (!loginForm || !emailInput || !errorMessage) {
+    console.error("Error: One or more elements are missing in the DOM.");
+  } else {
+    loginForm.addEventListener("submit", async (event) => { 
+      event.preventDefault();
 
     const email = emailInput instanceof HTMLInputElement ? emailInput.value : "";
-    const password = passwordInput instanceof HTMLInputElement ? passwordInput.value : ""; 
+    const password = passwordInput instanceof HTMLInputElement ? passwordInput.value : "";
 
-    try {
-      const data = await checkAuth(email, password);
-      if (data.token) { 
-        localStorage.setItem("authToken", data.token); // Store the token in localStorage
-        localStorage.setItem("isLoggedIn", "true"); // Store the login state
-        window.location.href = 'index.html'; // Redirection vers la page principale
+      try {
+        const data = await checkAuth(email, password);
+        if (data.token) { 
+          localStorage.setItem("authToken", data.token); // Store the token in localStorage
+          localStorage.setItem("isLoggedIn", "true"); // Store the login state
+          window.location.href = 'index.html'; // Redirection vers la page principale
+        }
+      } catch (error) {
+        console.error("Authentification error :", error);
+        errorMessage.textContent = "Email ou mot de passe incorrect.";
       }
-    } catch (error) {
-      console.error("Authentification error :", error);
-      errorMessage.textContent = "Email ou mot de passe incorrect.";
-    }
-  });
-} 
+    });
+  } 
+
+});
